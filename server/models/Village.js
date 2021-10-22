41cbc3512997e694430430fcb302ad5f480b67c0
LilyWS <stranger1212ss23@gmail.com>
2021-10-22 10:42:32 -0400

    Created mongoose schema

---
+++ server/models/Village.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+
+
+const villageSchema = new Schema({
+  population: {
+    type: Number,
+    required: true
+  },
+  abundanceOfResources: { //the multiplier that is applied whenver a resource is to be aqquired by this village (doesnt apply to trades) 
+    fruit: Number,
+    meat: Number,
+    gold: Number,
+    wood: Number
+  },
+  amountOfResources: {
+    fruit: Number,
+    meat: Number,
+    gold: Number,
+    wood: Number
+  },
+  unitAllocation: { //how many people do they have harvesting each resource?
+    fruit: Number,
+    meat: Number,
+    gold: Number,
+    wood: Number
+  },
+  user: { // the user tied to the village
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  trades: [ // the trades this village has up. Will only be used for display, actual trade logic will flow from the trade to the village
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Trade"
+    }
+  ]
+
+});
+
+const Village = model('Village', villageSchema);
+
+module.exports = Village;

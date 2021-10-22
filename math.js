335a3bd6bfb769e907338ff124256bfeff15123b
Keri Price <keri.lynne.p@gmail.com>
2021-10-22 16:19:20 -0400

    abundance randomizer and math util functions

--- math.js
+++ math.js
@@ -1,59 +1,34 @@
-// test variables
-const rate = 1;
-const population = 10;
-const fruit = {
-    amount: 5,
-    workers: 3,
-    abundance: 1.5
+function roundNum(num) {
+    return Math.round(num * 10) / 10;
 }
-const meat = {
-    amount: 3,
-    workers: 2,
-    abundance: 1.0
+
+function getRandom(max, min) {
+    return roundNum(Math.random() * (max - min) + min);
 }
-const lumber = {
-    amount: 2,
-    workers: 1,
-    abundance: 1.0
-}
-const gold = {
-    amount: 6,
-    workers: 4,
-    abundance: 0.8
+
+function getLimit(limit, max, saved) {
+    return (limit - saved < max) ? limit - saved : max;
 }
 
 // function to set abundance values
 function getAbundance() {
+    // abundance value range
     const max = 4.0;
     const min = 0.5;
-    // midpoint between all mins (2) and all maxs (12)
+    // the sum of all abundances, to balance villages
     const limit = 7.0;
 
-    const resource1 = Math.round((Math.random() * (max - min) + min) * 10) / 10;
-    const resource2 = 1;
+    const r1 = getRandom(max, min);
+    const r2 = getRandom(getLimit(limit, max, r1 + (min * 2)), min);
+    const r3 = getRandom(getLimit(limit, max, r1 + r2 + min), min);
+    const r4 = roundNum(limit - r1 - r2 - r3);
 
+    // checks that numbers are correctly balanced, reruns function if not
+    if(roundNum(r1 + r2 + r3 + r4) == limit && r4 <= max) {
+        return [r1, r2, r3, r4];
+    } else {
+        return getAbundance();
+    }
 }
 
-
-
-// this function will work together with a mutation to update the stored data
-function getResource(resource) {
-    const harvest = (rate * resource.workers) * resource.abundance;
-    resource.amount = Math.round((resource.amount + harvest) * 10) / 10;
-    return resource.amount;
-}
-
-// initial values
-console.log('fruit:', fruit.amount);
-console.log('meat:', meat.amount);
-console.log('lumber:', lumber.amount);
-console.log('gold:', gold.amount);
-console.log('=============');
-
-const timer = setInterval(function () {
-    console.log('fruit:', getResource(fruit));
-    console.log('meat:', getResource(meat));
-    console.log('lumber:', getResource(lumber));
-    console.log('gold:', getResource(gold));
-    console.log('=============');
-}, 2000);
+console.log(getAbundance());
--- server/gameLogic/index.js
+++ server/gameLogic/index.js
@@ -1,1 +1,46 @@
 //THIS FILE IS RESPONSIBLE FOR RUNNING LOGIC EVERY SECOND BASED ON THE PRODUCTION RATES OF EVERY VILLAGE
+// test variables - hard-coded temporarily
+const rate = 1;
+const population = 10;
+const fruit = {
+    amount: 5,
+    workers: 3,
+    abundance: 1.5
+}
+const meat = {
+    amount: 3,
+    workers: 2,
+    abundance: 1.0
+}
+const lumber = {
+    amount: 2,
+    workers: 1,
+    abundance: 1.0
+}
+const gold = {
+    amount: 6,
+    workers: 4,
+    abundance: 0.8
+}
+
+// this function will work together with a mutation to update the stored data
+function getResource(resource) {
+    const harvest = (rate * resource.workers) * resource.abundance;
+    resource.amount = Math.round((resource.amount + harvest) * 10) / 10;
+    return resource.amount;
+}
+
+// initial values
+console.log('fruit:', fruit.amount);
+console.log('meat:', meat.amount);
+console.log('lumber:', lumber.amount);
+console.log('gold:', gold.amount);
+console.log('=============');
+
+const addResources = setInterval(function () {
+    console.log('fruit:', getResource(fruit));
+    console.log('meat:', getResource(meat));
+    console.log('lumber:', getResource(lumber));
+    console.log('gold:', getResource(gold));
+    console.log('=============');
+}, 2000);

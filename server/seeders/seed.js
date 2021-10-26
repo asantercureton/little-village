20317be7ead9fcd79f68e07b244795262e173b7a
Keri Price <keri.lynne.p@gmail.com>
2021-10-26 00:41:16 -0400

    created test user seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -4,6 +4,7 @@ const userSeeds = require('./userSeeds.json');
 const villageSeeds = require('./villageSeeds.json');
 const tradeSeeds = require('./tradeSeeds.json')
 const levelSeeds = require('./levelSeeds.json');
+const { createVillage } = require('../utils/villageMethods');
 
 db.once('open', async () => {
   try {
@@ -15,6 +16,16 @@ db.once('open', async () => {
     await Trade.create(tradeSeeds);
     await Level.deleteMany({});
     await Level.create(levelSeeds);
+
+    // test users for each level
+    const seedsArr = ['tribe', 'hamlet', 'village', 'town', 'citystate', 'kingdom'];
+    for(let i = 0; i < seedsArr.length; i++) {
+      let user = await User.findOne({ username: seedsArr[i]});
+      let level = await Level.findOne({ level: i+1 });
+      user.village = await Village.create(createVillage(user, level));
+      await user.save();
+    }
+
   } catch (err) {
     console.error(err);
     process.exit(1);

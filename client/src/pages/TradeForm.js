1420d08e72772997636dc3937fba66211537f42d
Keri Price <keri.lynne.p@gmail.com>
2021-10-27 18:26:41 -0400

    partial function to tradeform

--- client/src/pages/TradeForm.js
+++ client/src/pages/TradeForm.js
@@ -1,32 +1,116 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import Auth from '../utils/auth';
+import React, { useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/client';
 
-
-
-import { QUERY_TRADES } from '../utils/queries';
-
-
+import { CREATE_TRADE } from '../utils/mutations';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 
 const TradeForm = () => {
-    const { loading, data } = useQuery(QUERY_TRADES);
-    const trades = data?.trades || [];
-    console.log('trade', trades);
-    // const renderUserList = () => {
-    //   if (loading) {
-    //     return <h2>Loading...</h2>
-    //   } else {
-    //     return <UserList users={users} title="List of Users" />
-    //   }
-    // }
+    const { id } = useParams();
 
-    const renderTrade = () => {
-        if (!Auth.loggedIn()) return null;
-        return Auth.getTrade().data.trades;
-    }
-    console.log('trade', trades);
+    const { loading, data, error } = useQuery(id ? QUERY_USER : QUERY_ME, {
+        variables: { id },
+    });
 
+    const user = data?.me || data?.user || {};
+
+    const [formState, setFormState] = useState({
+        userId: user._id,
+        resourceSold: '',
+        amountSold: '',
+        resourceBought: '',
+        amountBought: '',
+        tradeAmount: ''
+    });
+
+    const [createTrade, { error: tradeError, data: tradeData }] = useMutation(CREATE_TRADE);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { tradeData } = await createTrade({
+                variables: { ...formState },
+            });
+
+        } catch (e) {
+            console.error(e);
+        }
+        // clear form values
+        setFormState({
+            userId: user._id,
+            resourceSold: '',
+            amountSold: '',
+            resourceBought: '',
+            amountBought: '',
+            tradeAmount: ''
+        });
+    };
+
+    const renderForm = () => {
+        if (tradeData) {
+            return (
+                <div>
+                    <h2> Trade Created!</h2>
+                    <p>Make Another Offer?</p>
+                </div>
+            )
+        }
+        return (
+            <form onSubmit={handleFormSubmit}>
+                <div className="form-group">
+                    <div>
+                        <label for="exampleInputEmail1">Resource Offered: </label>
+                    </div>
+                    <select name="resourceSold" value={formState.resourceSold} onChange={handleChange} className="form-control-sm" id="exampleFormControlSelect1">
+                        <option value='fruit'>Fruit</option>
+                        <option value='meat'>Meat</option>
+                        <option value='gold'>Gold</option>
+                        <option value='wood'>Wood</option>
+                    </select>
+
+                    <div>
+                        <label className="amount" for="exampleInputEmail1">Amount Offered:</label>
+                    </div>
+                    <input name="amountSold" value={formState.amountSold} onChange={handleChange} type="text" inputmode="decimal" placeholder="" />
+
+                </div>
+                <hr />
+                <div>
+                    <label for="exampleInputEmail1">Resource Requested: </label>
+                </div>
+                <select name="resourceBought" value={formState.resourceBought} onChange={handleChange} className="form-control-sm" id="exampleFormControlSelect1">
+                    <option>Fruit</option>
+                    <option>Meat</option>
+                    <option>Gold</option>
+                    <option>Wood</option>
+                </select>
+
+                <div>
+                    <label className="amount" for="exampleInputEmail1">Amount Requested:</label>
+                </div>
+                <input name="amountBought" value={formState.amountBought} onChange={handleChange} type="text" inputmode="decimal" placeholder="" />
+                <hr />
+                <div className="multiplier">
+                    <div>
+                        <label className="labelTrade">Trade Multiplier:</label>
+                        <input name="tradeAmount" value={formState.tradeAmount} onChange={handleChange} className="xbox" type="text" inputmode="decimal" placeholder="" />
+                    </div>
+                    <label>(Keep field blank for infinite trades)</label>
+                </div>
+                <button type="submit" className="btn createTrade-btn">OFFER TRADE</button>
+            </form>
+        );
+    };
 
     return (
         <main>
@@ -34,48 +118,8 @@ const TradeForm = () => {
                 <div className="row2">
                     <div className="villageCard">
                         <h1 className="tradeTitle">CREATE A TRADE!</h1>
-                        <form>
-                            <div className="form-group">
-                                <div>
-                                    <label for="exampleInputEmail1">Resource Requested: </label>
-                                </div>
-                                <select className="form-control-sm" id="exampleFormControlSelect1">
-                                    <option>Fruit</option>
-                                    <option>Meat</option>
-                                    <option>Gold</option>
-                                    <option>Wood</option>
-                                </select>
-
-                                <div>
-                                    <label className="amount" for="exampleInputEmail1">Amount Requested:</label>
-                                </div>
-                                <input type="text" inputmode="decimal" placeholder="" />
-                                <hr />
-                                <div>
-                                    <label for="exampleInputEmail1">Resource Offered: </label>
-                                </div>
-                                <select className="form-control-sm" id="exampleFormControlSelect1">
-                                    <option>Fruit</option>
-                                    <option>Meat</option>
-                                    <option>Gold</option>
-                                    <option>Wood</option>
-                                </select>
-
-                                <div>
-                                    <label className="amount" for="exampleInputEmail1">Amount Offered:</label>
-                                </div>
-                                <input type="text" inputmode="decimal" placeholder="" />
-
-                            </div>
-                            <div className="multiplier">
-                                <div>
-                                    <label className="labelTrade">Trade Multiplier:</label>
-                                    <input className="xbox" type="text" inputmode="decimal" placeholder="" />
-                                </div>
-                                <label>(Keep field blank for infinite trades)</label>
-                            </div>
-                            <button type="submit" className="btn createTrade-btn">TRADE</button>
-                        </form>
+                        {renderForm()}
+                        {error && <div>{error.message}</div>}
                     </div>
                 </div>
             </div>

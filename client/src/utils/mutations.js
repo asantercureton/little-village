b3767c23c4a99f1fc986f584fab39ba95825db14
LilyWS <stranger1212ss23@gmail.com>
2021-10-28 16:06:39 -0400

    debugging menu

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -86,15 +86,9 @@ export const GET_UPDATED_RESOURCES = gql`
   }
 `;
 
-<<<<<<< HEAD
 export const EXECUTE_TRADE = gql`
   mutation executeTrade($userId: ID!, $tradeId: ID!) {
     executeTrade(userId: $userId, tradeId: $tradeId) {
-=======
-export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
->>>>>>> 38409c8 (got client display)
       _id
     }
   }
